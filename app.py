03eb4a3dd5172883466e9ecd4146f52942b8f04e
Your Name <moritz.schoettle@studmail.hs-aalen.de>
2023-10-25 19:59:01 +0200

    feat: Algorithm für Heimvorteile mit Erklärung

--- app.py
+++ app.py
@@ -2,54 +2,98 @@ import json
 import streamlit as st 
 import pandas as pd
 
-def provide_raw_data(data: dict)-> None:
+def provide_raw_data(path):
+    with open(file=path, mode="r") as raw_file:
+        raw_data = json.load(raw_file)
+
+    raw_data_df = pd.DataFrame(raw_data["games"])
+
+    home_team = st.selectbox(label="Home Team", options=raw_data["teams"], index=0)
+    away_team = st.selectbox(label="Away Team", options=raw_data["teams"], index=1)
 
     with st.expander(label="Raw Data"):
-        st.json(data)
+        st.json(raw_data)
 
+    return raw_data_df, home_team, away_team
 
-    return
-
-def provide_derived_data(df: pd.DataFrame) -> None: 
+def provide_derived_data(raw_data_df):
     with st.expander(label="Insights"):
-
-        st.subheader("Home Insights") 
-        home_stats = df.groupby("team") [["points_scored", "points_allowed"]].mean()
+        st.subheader("Home Insights")
+        home_stats = raw_data_df.groupby("team")[["points_scored", "points_allowed"]].mean()
         home_stats["team"] = home_stats.index
         home_stats.sort_values("points_scored", ascending=False, inplace=True)
         home_stats.reset_index(drop=True, inplace=True)
-        home_stats.index +=1 
+        home_stats.index += 1
         st.write(home_stats)
 
-        st.subheader("Away Insights") 
-        away_stats = df.groupby("opponent") [["points_scored", "points_allowed"]].mean()
+        st.subheader("Away Insights")
+        away_stats = raw_data_df.groupby("opponent")[["points_scored", "points_allowed"]].mean()
         away_stats["team"] = away_stats.index
         away_stats.sort_values("points_scored", ascending=False, inplace=True)
         away_stats.reset_index(drop=True, inplace=True)
-        away_stats.index +=1 
+        away_stats.index += 1
         st.write(away_stats)
+    return home_stats, away_stats
+
+def provide_algorithm(raw_data_df):
+    with st.expander("Algorithm for Home Advantage"):
+        st.markdown(
+            """
+                    **Heimvorteil sind +3 Punkte im Handicap***(Modellierte Annahme)*
+                    
+                    ---
+                    Erklärung:
+                    Die Schätzung des Heimvorteils in der NFL auf ungefähr 2,5 bis
+                    3 Punkte pro Spiel basiert auf einer Kombination von historischen Daten, Studien
+                    und Erfahrungen von Sportanalysten. Es ist wichtig zu beachten, dass dies eine
+  allgemeine Schätzung ist und keine exakte wissenschaftliche Berechnung darstellt.
+ Hier sind einige der Quellen und Grundlagen, auf denen diese Schätzung basiert:
+
+                    - **Historische Daten:** Durch die Analyse von jahrzehntelangen
+NFL-Spielprotokollen können Sportanalysten Muster erkennen, die darauf hinweisen,
+dass Teams, die zu Hause spielen, tendenziell bessere Ergebnisse erzielen als bei
+Auswärtsspielen. Dies kann als Ausgangspunkt für die Schätzung des Heimvorteils
+dienen.
+
+                    - **Akademische Studien:** Es gibt einige akademische Studien
+und wissenschaftliche Arbeiten, die den Heimvorteil im Sport, einschließlich der
+NFL, untersuchen. Diese Studien nutzen statistische Methoden, um den Heimvorteil zu
+quantifizieren. Obwohl die Ergebnisse variieren können, zeigen viele dieser Studien
+einen Heimvorteil von etwa 2,5 bis 3 Punkten pro Spiel.
+
+
+                    - **Erfahrung von Sportanalysten:** Sportexperten und
+Analysten, die die NFL und andere Sportligen abdecken, bringen ihre Erfahrung und
+Einsichten in die Schätzung des Heimvorteils ein. Dies kann auf beobachteten
+Mustern und ihrer Kenntnis der Dynamik von Heim- und Auswärtsspielen basieren.
+"""
+        )
+
+        prepared_data_df = raw_data_df.copy()
+
+        condition_1 = raw_data_df["points_scored"] - raw_data_df["points_allowed"] > 3
+        condition_2 = raw_data_df["points_scored"] - raw_data_df["points_allowed"] < 0
+        prepared_data_df["true wins"] = condition_1 | condition_2
+        st.write(prepared_data_df)
+    return
 
 
 
 
-    return 
 
 def main(): 
     st.title("NFL-Predictor")
 
-    with open(file="data/nfl_data.json", mode="r") as raw_file: 
-        raw_data = json.load(raw_file)
-
-    raw_data_df= pd.DataFrame(raw_data["games"])
-
-    st.selectbox(label="Home Teams", options=raw_data["teams"], index=0)
-    st.selectbox(label="Away Teams", options=raw_data["teams"], index=1)
+    path = "data/nfl_data.json"
     
     #Level 1 
-    provide_raw_data(data=raw_data)
-
+    raw_data_df, home_team, away_team = provide_raw_data(path=path)
+    
     #Level 2
-    provide_derived_data(df=raw_data_df)
+    home_stats, away_stats = provide_derived_data(raw_data_df=raw_data_df) 
+
+    # Level 3
+    provide_algorithm(raw_data_df=raw_data_df)
 
     return 
 
