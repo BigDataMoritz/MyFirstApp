f520412a671497234f98444de9c6b819eb873142
Your Name <moritz.schoettle@studmail.hs-aalen.de>
2023-12-13 18:49:50 +0100

    feat: update skript

--- crawler/webscraper.py
+++ crawler/webscraper.py
@@ -1,4 +1,5 @@
-import json 
+import json
+import time 
 from bs4 import BeautifulSoup
 from requests import Session, RequestException
 from rich import print
@@ -16,7 +17,6 @@ def request_url(session: Session, url: str, retries:int =3, timeout: int = 10 ):
         try:
             response = session.get(url=url, timeout=timeout,  headers=HEADERS)
             response.raise_for_status() 
-
             return response.text
 
        
@@ -34,17 +34,46 @@ def main():
     boxscore_links = read_json(path=INPUT_PATH)
     http_session=Session()
 
-    url= "https://www.pro-football-reference.com/boxscores/202309110nyj.htm"
-    html = request_url(session=http_session, url=url)
-    soup =  parse_html_to_soup(html)
-    table = soup.find("table", class_="linescore")
-    
-    print(table)
+    # url= "https://www.pro-football-reference.com/boxscores/202309110nyj.htm"
 
-    # for key, urls in boxscore_links.items():
-    #     print(key)
+    scraped_data = dict()
 
-    #     for urls in links: 
+    for key, urls in boxscore_links.items():
+
+        payload_data = list()
+        for url in urls:
+            time.sleep(2) 
+            html = request_url(session=http_session, url=url)
+            soup =  parse_html_to_soup(html)
+            table = soup.find("table", class_="linescore")
+        
+            rows = table.select ("table tbody tr")
+
+            teams = []
+            for row in rows:
+                table_data_cells = row.find_all(["td"])
+                team_name = table_data_cells[1].text
+                point_cells = table_data_cells[2:]
+                points = [int(cell.text) for cell in point_cells]
+                final_points = points [-1]
+                quater_points = points [:-1]
+                if not (final_points == sum(quater_points)):
+                    raise ValueError("Quarter Pouns do not match final points")
+
+                result = (team_name, final_points)
+                teams.append(result)
+
+            data = {
+                "team": teams[1][0],
+                "opponent": teams[0][0],
+                "points_scored": teams[1][1],
+                "points_allowed": teams[0][1],
+                }
+                
+            payload_data.append(data)
+        scraped_data[key] = payload_data
+        print(scraped_data)
+
     return 
 
 if __name__ == "__main__": 
